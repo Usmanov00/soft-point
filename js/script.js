9e7c6d7614ae763b4088c2bb1d83ca089c422501
Usmanov <roma.maksato@gmail.com>
2022-01-14 02:52:04 +0300

    added price section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 const range = document.getElementById('range');
 
 range.addEventListener('input', (e) => {
@@ -26,3 +27,5 @@ range.addEventListener('input', (e) => {
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
+
+
