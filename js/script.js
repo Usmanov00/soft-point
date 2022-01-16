f257757fd07564e013c902fc0ce315325dfee7b6
Usmanov <roma.maksato@gmail.com>
2022-01-17 00:59:00 +0300

    added burger menu

--- js/script.js
+++ js/script.js
@@ -48,4 +48,19 @@ $(document).ready(function(){
     });
 });
 
+const muBurger = document.querySelector('.burger')
+const myMenu = document.querySelector('.menu')
+
+muBurger.addEventListener('click', () => {
+    myMenu.classList.toggle('show')
+    document.body.classList.toggle('o-hidden')
+})
+
+menuLinks.forEach(oneLink => {
+    oneLink.addEventListener('click', () => {
+        myMenu.classList.remove('show')
+        document.body.classList.toggle('o-hidden')
+    })
+})
+
 
